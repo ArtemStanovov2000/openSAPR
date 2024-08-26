975473176f8e6d46414cd93771e4a91d79f6373f
g7g7g8 <artemstanovov36@gmail.com>
2024-08-26 13:48:30 +0300

    Последнее изменение 26.08.2024

--- src/utils/rerender/rerender.ts
+++ src/utils/rerender/rerender.ts
@@ -1,7 +1,6 @@
 import { drawLine } from "../drawLine/drawLine"
 import { drawCircle } from "../drawCircle/drawCircle"
 import { drawFigure } from "./drawFigure"
-import { zoom } from "../zoom/zoom"
 
 export const data: any = []
 
@@ -9,5 +8,4 @@ export const rerender = (context: any) => {
    drawCircle(context)
    drawLine(context)
    drawFigure(context)
-   zoom()
 }
